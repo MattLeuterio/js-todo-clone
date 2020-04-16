f6a871b794eec9743aa6fb829ab0a99e4568b6e9
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-04-16 17:19:14 +0200

    Html+Css+Js done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,170 @@
 console.log('ok');
 console.log('jQuery OK -->', $);
+
+$(document).ready( function() {
+
+/************************************
+ * 
+ * TODO LIST 
+ * 
+ * Ricreare la todo list vista oggi durante la lezione (focus dell’esercizio la logica)
+ * Usiamo i template in html e il clone per separare la parte logica (JS)
+ *  dalla parte di markup (HTML)
+ * recap funzionalità: 
+ * mostrare, 
+ * aggiungere
+ * eliminare elementi dalla lista
+ * 
+ ************************************/
+
+// Setup
+
+// Ref funzionalità
+var listStart = [
+    'Corso - 9.30-15.00',
+    'Esercizio',
+    'Controllare Email',
+    'Vincere al superenalotto'
+]
+var listContainer = $('.list-container');
+var input = $('.input-section input');
+
+// Ref Data
+var dateNow = new Date();
+var day = '';
+var month = '';
+var dateContainer = $('.left-header .today-date');
+
+
+// Aggiornare data corrente
+
+// Switch per associare il giorno testuale al numerico
+switch ( new Date().getDay() ) {
+         case 0: 
+             day = 'Sunday'
+            break;
+         case 1: 
+             day = 'Monday'
+             break;
+         case 2: 
+             day = 'Tuesday'
+             break;
+         case 3: 
+             day = 'Wednesday'
+             break;
+         case 4: 
+             day = 'Thursday'
+             break;
+         case 5: 
+             day = 'Friday'
+             break;
+         case 6: 
+             day = 'Saturday'
+             break;
+         default:
+             day = false;
+      };
+
+// Switch per associare il mese testuale al numerico
+switch ( new Date().getMonth() ) {
+        case 0: 
+            month = 'January'
+           break;
+        case 1: 
+            month = 'February'
+            break;
+        case 2: 
+            month = 'March'
+            break;
+        case 3: 
+            month = 'April'
+            break;
+        case 4: 
+            month = 'May'
+            break;
+        case 5: 
+            month = 'June'
+            break;
+        case 6: 
+            month = 'July'
+            break;
+        case 7: 
+            month = 'August'
+            break;
+        case 8: 
+            month = 'September'
+            break;
+        case 9: 
+            month = 'October'
+            break;
+        case 10: 
+            month = 'November'
+            break;
+        case 11: 
+            month = 'December'
+            break;
+        default:
+            month = false;
+     };      
+  
+dateContainer.text(day + ', ' + month + ' ' + dateNow.getDate() );
+
+
+//Popolazione Lista
+for(var i = 0; i < listStart.length; i++) {
+    //Cloniamo il template
+    var listTaskClone = $('.template-task li').clone();
+    
+    //Inseriamo il testo
+    listTaskClone.prepend(listStart[i]);
+    
+    //Inseriamo nella lista
+    listContainer.append(listTaskClone);
+};
+
+// Cancellare elemento dalla lista con metodo ON()
+
+$('body').on('click', '.todo-list ul li a', function() {
+    $(this).parent().remove();
+});
+
+// Aggiungere elemento tramite input
+
+// Cliccando il tasto Invio
+$('.input-section input').keyup(function(event) {
+
+    if(event.keyup == 13 || event.keyCode == 13) {
+        //Cloniamo il template
+        var listTaskClone = $('.template-task li').clone();
+        //Inseriamo il testo
+        listTaskClone.prepend(input.val());
+        //Inseriamo nella lista
+        listContainer.append(listTaskClone);
+        //Puliamo il campo input
+        input.val('')
+    };
+});
+
+// Cliccando il Bottone "Add Task"
+$('.input-section button').click(function(event) {
+        //Cloniamo il template
+        var listTaskClone = $('.template-task li').clone();
+        //Inseriamo il testo
+        listTaskClone.prepend(input.val());
+        //Inseriamo nella lista
+        listContainer.append(listTaskClone);
+        //Puliamo il campo input
+        input.val('')
+
+});
+
+// Click sull'elemento per renderlo completato
+
+$('body').on('click', '.todo-list ul li', function() {
+    $(this).addClass('completed');
+});
+
+
+
+
+}); // <- End Doc Ready!
